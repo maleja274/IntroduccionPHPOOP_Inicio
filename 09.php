e8a42b38b08105621e01364fbc15e2c733f1c0ad
maleja274 <maleja274@hotmail.com>
2022-09-27 22:04:51 -0500

    php.final.objetos

--- 09.php
+++ 09.php
@@ -4,7 +4,28 @@
 
 $db = new mysqli('localhost', 'root', 'loreto.2', 'bienesraices_crud');
 
-var_dump($db);
+// creamos el query
+$query = "SELECT titulo, imagen, precio, descripcion from propiedades";
+
+// lo preparamos
+$stmt = $db->prepare($query);
+
+// lo ejecutamos
+$stmt->execute();
+
+// creamos la variable
+$stmt->bind_result($titulo, $imagen, $precio, $descripcion);
+
+//asignamos el resultado
+// $stmt->fetch();
+
+//imprimir el resultado
+while ($stmt->fetch()):
+var_dump($titulo);
+var_dump($imagen);
+var_dump($precio);
+var_dump($descripcion);
+endwhile;
 
 
 include 'includes/footer.php';
--- 10.php
+++ 10.php
@@ -1,4 +1,27 @@
 <?php include 'includes/header.php';
 
+// Conectar a la BD con PDO
+$db = new PDO('mysql:host=localhost; dbname=bienesraices_crud', 'root', 'loreto.2');
+
+// Creamos el query
+$query = "SELECT titulo, imagen from propiedades";
+
+// Lo preparamos
+$stmt = $db->prepare($query);
+
+// Lo ejecutamos
+$stmt->execute();
+
+// Obtener los resultados
+$resultado = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+// Iterar
+foreach($resultado as $propiedad):
+    echo $propiedad['titulo'];
+    echo "<br>";
+    echo $propiedad['imagen'];
+    echo "<br>";
+endforeach;
+
 
 include 'includes/footer.php';
